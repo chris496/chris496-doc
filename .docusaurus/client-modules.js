4a3e9aa87285c15de5087d6d1f7bf7ed7d297e55
Christophe Dumas <cdumas@servitex.fr>
2022-07-27 22:38:48 +0200

    test

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('C:\\Users\\cdumas\\Desktop\\formation\\chris496-doc\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('C:\\Users\\cdumas\\Desktop\\formation\\chris496-doc\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('C:\\Users\\cdumas\\Desktop\\formation\\chris496-doc\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('C:\\Users\\cdumas\\Desktop\\formation\\chris496-doc\\src\\css\\custom.css'),
-];
