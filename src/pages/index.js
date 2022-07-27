3fe9c5b6a145cf0857f500d949007f7bb295a2f1
Christophe Dumas <cdumas@servitex.fr>
2022-07-27 22:49:34 +0200

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/doc_chris496/demo">
+            to="/docs/OC/demo">
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
